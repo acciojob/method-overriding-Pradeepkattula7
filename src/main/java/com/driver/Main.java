d5dfe4c1ba0e9d0673a5ac7374cfe3d9af5c3818
Pradeep Kattula <pradeepkattula390@gmail.com>
2023-10-21 10:15:09 +0530

    one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,18 +5,15 @@ class A{
     }
 }
 class B extends A{
-    void method(){
-        super.meth();
-    }
-   String meth(){
+    String meth(){
         return "Method is overridden in Extendend class B";
-   }
+    }
 }
 public class Main {
     public static void main(String[] args) {
-        B b = new B();
-       b.method();
-       b.meth();
-    }
+        new B().meth();
+        B b=new B();
+        b.meth();
 
+    }
 }
